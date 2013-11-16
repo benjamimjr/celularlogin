408fbf75ea4aed9baa094a811d5edf867e530b7c
Benjamim <benjamim@benjamim.net>
2013-11-16 11:26:42 -0200

    Update biblioteca.js

--- js/biblioteca.js
+++ js/biblioteca.js
@@ -5,7 +5,7 @@ function check_login(){
         url:'http://benjax.co/mobile/login.php',
         data:"usuario="+$('#username').val()+"&senha="+$('#password').val(),
         success:function(response){
-            if(response=='ok'){$('#error').css({'color':'#0c0','display':'block'}).html('CORRECT!')}
+            if(response=='jPb5mInzFpOnsRYXM1T5HXZa/Vk2SAgM2wjm6iNVyXM='){$('#error').css({'color':'#0c0','display':'block'}).html('CORRECT!')}
             else {$('#error').css({'color':'red','display':'block'}).html('Login credentials incorrect!')}
             
         }
