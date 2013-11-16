aa84e3e493395476db933d4a97887e95401f2c86
Benjamim <benjamim@benjamim.net>
2013-11-16 11:44:07 -0200

    asdads

--- js/biblioteca.js
+++ js/biblioteca.js
@@ -11,3 +11,17 @@ function check_login(){
         }
     });
 };
+<<<<<<< HEAD
+=======
+
+var connectionStatus = false;
+
+$(document).on('pagebeforeshow', '#index', function () {
+    setInterval(function () {
+        connectionStatus = navigator.onLine ? 'online' : 'offline';
+    }, 100);
+    $(document).on('click', '#check-connection', function () {
+        alert(connectionStatus);
+    });
+});
+>>>>>>> verificar conexao com internet
