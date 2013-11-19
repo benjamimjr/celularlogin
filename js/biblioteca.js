5a9ab05595101de18d1551da736cfe1e06acd3d5
Benjamim <benjamim@benjamim.net>
2013-11-19 15:19:00 -0200

    Ajuste do crossdomain

--- js/biblioteca.js
+++ js/biblioteca.js
@@ -3,6 +3,8 @@ function check_login(){
     $.ajax({
         type:'POST',
         url:'http://benjax.co/mobile/login.php',
+        contentType: 'application/json; charset=utf-8',
+        dataType:'jsonp',
         data:"usuario="+$('#username').val()+"&senha="+$('#password').val(),
         success:function(response){
             if(response=='jPb5mInzFpOnsRYXM1T5HXZa/Vk2SAgM2wjm6iNVyXM='){$('#error').css({'color':'#0c0','display':'block'}).html('CORRECT!')}
